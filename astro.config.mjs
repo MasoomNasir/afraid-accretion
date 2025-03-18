d5a492b6becccb4183a7bd3232a239cb3cca5bf1
Masoom Nasir <masoomnasir60@gmail.com>
2025-03-18 13:05:36 +0500

    Fix import errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+// import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [
-    tailwind({
-      // Enable full dev UI classes
-      applyBaseStyles: false,
-    }),
-    react(),
+    // tailwind({
+    //   // Enable full dev UI classes
+    //   // applyBaseStyles: false,
+    // }),
+    // react(),
   ],
 });
